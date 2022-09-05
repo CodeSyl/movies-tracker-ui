d763014ac1cf0e9a282c320cf628a515457538aa
syl <sylvain.lapart@gmail.com>
2022-09-05 08:45:33 +0200

    :wrench: Update config to sync baseweb with nextjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Client as Styletron } from "styletron-engine-atomic";
+import { Provider as StyletronProvider } from "styletron-react";
+import { LightTheme, BaseProvider } from "baseui";
+import { styletron } from "../styletron";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <StyletronProvider value={styletron}>
+      <BaseProvider theme={LightTheme}>
+        <Component {...pageProps} />
+      </BaseProvider>
+    </StyletronProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
+import { ArrowUp } from "baseui/icon";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <h1>hello</h1>
+    </div>
+  );
 };
 
 export default Home;
